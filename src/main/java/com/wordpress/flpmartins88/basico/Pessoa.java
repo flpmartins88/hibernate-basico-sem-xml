4f1ef78dfd2fa68128acfbd7939c62f2a56712c9
Felipe Martins <felipe_7@msn.com>
2014-09-03 21:26:23 -0300

    Criação do projeto

---
+++ src/main/java/com/wordpress/flpmartins88/basico/Pessoa.java
@@ -0,0 +1,37 @@
+package com.wordpress.flpmartins88.basico;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="pessoa")
+public class Pessoa implements Serializable {
+
+	@Id @GeneratedValue
+	private Long id;
+	
+	private String nome;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	
+}
